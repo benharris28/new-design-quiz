0cbcd4fa5ce7d42b32387f1aeef687e2ed4aafda
Benjamin Harris <benjaminharris@Benjamins-MacBook-Air.local>
2023-12-06 21:14:16 -0500

    first update

---
+++ app/quiz/page.tsx
@@ -0,0 +1,58 @@
+// pages/quiz.tsx
+'use client'
+import { useState } from 'react';
+import websiteData from '../../data.json'; // Adjust the path to your JSON file
+
+interface Website {
+    websiteName: string;
+  screenshotUrl: string;
+}
+
+interface Response {
+  website: string;
+  response: string;
+}
+
+//submit responses from state and array of websites in json file to openAI assistant API
+
+const Quiz: React.FC = () => {
+  const [currentWebsiteIndex, setCurrentWebsiteIndex] = useState<number>(0);
+  const [userResponses, setUserResponses] = useState<Response[]>([]);
+  const [hasResponded, setHasResponded] = useState<boolean>(false);
+
+  const handleResponse = (likeOrDislike: string) => {
+    setUserResponses([...userResponses, { website: websiteData[currentWebsiteIndex].websiteName, response: likeOrDislike }]);
+    setHasResponded(true);
+  };
+
+  const loadNextWebsite = () => {
+    if (currentWebsiteIndex < websiteData.length - 1) {
+      setCurrentWebsiteIndex(currentWebsiteIndex + 1);
+      setHasResponded(false);
+    } else {
+      // Submit data to OpenAI here or set state to show results
+    }
+  };
+
+  const isLastWebsite = currentWebsiteIndex === websiteData.length - 1;
+
+  return (
+    <div className="p-4">
+      <p className="text-lg mb-4">Please scroll through this webpage and tell us if you like it.</p>
+      <img src={websiteData[currentWebsiteIndex].screenshotUrl} alt="Website Screenshot" className="w-full h-auto mb-16" />
+      <div className="fixed inset-x-0 bottom-0 bg-white p-4 shadow-md">
+        <p className="text-lg">Do you like this website?</p>
+        <button className="bg-green-500 text-white py-2 px-4 rounded mr-4" onClick={() => handleResponse('like')}>Yes</button>
+        <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={() => handleResponse('dislike')}>No</button>
+        {hasResponded && !isLastWebsite && 
+          <button className="bg-blue-500 text-white py-2 px-4 rounded ml-4" onClick={loadNextWebsite}>Next Website</button>
+        }
+        {hasResponded && isLastWebsite && 
+          <button className="bg-blue-500 text-white py-2 px-4 rounded ml-4" onClick={loadNextWebsite}>Show me results</button>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
